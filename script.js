2f71abe2268cdf3c2cd36996d6b1acd428e44a33
Chandrakant Budhalakoti <121392931+ChandrakantBudhalakoti@users.noreply.github.com>
2023-04-04 10:15:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,206 @@
+//taking element form user===>>>
+function calculateTotal() {
+  const price1 = parseFloat(document.getElementById("price1").value);
+  const price2 = parseFloat(document.getElementById("price2").value);
+  const price3 = parseFloat(document.getElementById("price3").value);
+  const price4 = parseFloat(document.getElementById("price4").value);
+  const price5 = parseFloat(document.getElementById("price5").value);
+  const price6 = parseFloat(document.getElementById("price6").value);
+  const price7 = parseFloat(document.getElementById("price7").value);
+  const price8 = parseFloat(document.getElementById("price8").value);
+  const price9 = parseFloat(document.getElementById("price9").value);
+  const price10 = parseFloat(document.getElementById("price10").value);
+  const price11 = parseFloat(document.getElementById("price11").value);
+  const price12 = parseFloat(document.getElementById("price12").value);
+  const price13 = parseFloat(document.getElementById("price13").value);
+  const price14 = parseFloat(document.getElementById("price14").value);
+  const price15 = parseFloat(document.getElementById("price15").value);
+  const price16 = parseFloat(document.getElementById("price16").value);
+  const price17 = parseFloat(document.getElementById("price17").value);
+  const price18 = parseFloat(document.getElementById("price18").value);
+  const price19 = parseFloat(document.getElementById("price19").value);
+  const price20 = parseFloat(document.getElementById("price20").value);
+  const price21 = parseFloat(document.getElementById("price21").value);
+  const price22 = parseFloat(document.getElementById("price22").value);
+  const price23 = parseFloat(document.getElementById("price23").value);
+  const price24 = parseFloat(document.getElementById("price24").value);
+  const price25 = parseFloat(document.getElementById("price25").value);
+  const price26 = parseFloat(document.getElementById("price26").value);
+  const price27 = parseFloat(document.getElementById("price27").value);
+  const price28 = parseFloat(document.getElementById("price28").value);
+  const price29 = parseFloat(document.getElementById("price29").value);
+  const price30 = parseFloat(document.getElementById("price30").value);
+  const price31 = parseFloat(document.getElementById("price31").value);
+  const price32 = parseFloat(document.getElementById("price32").value);
+  const price33 = parseFloat(document.getElementById("price33").value);
+  const price34 = parseFloat(document.getElementById("price34").value);
+  const price35 = parseFloat(document.getElementById("price35").value);
+  const price36 = parseFloat(document.getElementById("price36").value);
+  const price37 = parseFloat(document.getElementById("price37").value);
+  const price38 = parseFloat(document.getElementById("price38").value);
+  const price39 = parseFloat(document.getElementById("price39").value);
+  const price40 = parseFloat(document.getElementById("price40").value);
+  const price41 = parseFloat(document.getElementById("price41").value);
+  const price42 = parseFloat(document.getElementById("price42").value);
+  const price43 = parseFloat(document.getElementById("price43").value);
+  const price44 = parseFloat(document.getElementById("price44").value);
+  const price45 = parseFloat(document.getElementById("price45").value);
+  const price46 = parseFloat(document.getElementById("price46").value);
+  const price47 = parseFloat(document.getElementById("price47").value);
+  const price48 = parseFloat(document.getElementById("price48").value);
+  const price49 = parseFloat(document.getElementById("price49").value);
+  const price50 = parseFloat(document.getElementById("price50").value);
+  const price51 = parseFloat(document.getElementById("price51").value);
+  const price52 = parseFloat(document.getElementById("price52").value);
+  const price53 = parseFloat(document.getElementById("price53").value);
+  const price54 = parseFloat(document.getElementById("price54").value);
+  const price55 = parseFloat(document.getElementById("price55").value);
+  const price56 = parseFloat(document.getElementById("price56").value);
+  const price57 = parseFloat(document.getElementById("price57").value);
+  const price58 = parseFloat(document.getElementById("price58").value);
+  const price59 = parseFloat(document.getElementById("price59").value);
+  const price60 = parseFloat(document.getElementById("price60").value);
+  const price61 = parseFloat(document.getElementById("price61").value);
+  const price62 = parseFloat(document.getElementById("price62").value);
+
+  //adding price to calculate the sum===>>>
+  const total1 = parseFloat(price1) + parseFloat(price2);
+  const total2 = parseFloat(price3) + parseFloat(price4);
+  const total3 = parseFloat(price5) + parseFloat(price6);
+  const total4 = parseFloat(price7) + parseFloat(price8);
+  const total5 = parseFloat(price9) + parseFloat(price10);
+  const total6 = parseFloat(price11) + parseFloat(price12);
+  const total7 = parseFloat(price13) + parseFloat(price14);
+  const total8 = parseFloat(price15) + parseFloat(price16);
+  const total9 = parseFloat(price17) + parseFloat(price18);
+  const total10 = parseFloat(price19) + parseFloat(price20);
+  const total11 = parseFloat(price21) + parseFloat(price22);
+  const total12 = parseFloat(price23) + parseFloat(price24);
+  const total13 = parseFloat(price25) + parseFloat(price26);
+  const total14 = parseFloat(price27) + parseFloat(price28);
+  const total15 = parseFloat(price29) + parseFloat(price30);
+  const total16 = parseFloat(price31) + parseFloat(price32);
+  const total17 = parseFloat(price33) + parseFloat(price34);
+  const total18 = parseFloat(price35) + parseFloat(price36);
+  const total19 = parseFloat(price37) + parseFloat(price38);
+  const total20 = parseFloat(price39) + parseFloat(price40);
+  const total21 = parseFloat(price41) + parseFloat(price42);
+  const total22 = parseFloat(price43) + parseFloat(price44);
+  const total23 = parseFloat(price45) + parseFloat(price46);
+  const total24 = parseFloat(price47) + parseFloat(price48);
+  const total25 = parseFloat(price49) + parseFloat(price50);
+  const total26 = parseFloat(price51) + parseFloat(price52);
+  const total27 = parseFloat(price53) + parseFloat(price54);
+  const total28 = parseFloat(price55) + parseFloat(price56);
+  const total29 = parseFloat(price57) + parseFloat(price58);
+  const total30 = parseFloat(price59) + parseFloat(price60);
+  const total31 = parseFloat(price61) + parseFloat(price62);
+
+  //showing total===>>>
+
+  document.getElementById("total1").textContent = total1.toFixed(2);
+  document.getElementById("total2").textContent = total2.toFixed(2);
+  document.getElementById("total3").textContent = total3.toFixed(2);
+  document.getElementById("total4").textContent = total4.toFixed(2);
+  document.getElementById("total5").textContent = total5.toFixed(2);
+  document.getElementById("total6").textContent = total6.toFixed(2);
+  document.getElementById("total7").textContent = total7.toFixed(2);
+  document.getElementById("total8").textContent = total8.toFixed(2);
+  document.getElementById("total9").textContent = total9.toFixed(2);
+  document.getElementById("total11").textContent = total10.toFixed(2);
+  document.getElementById("total10").textContent = total11.toFixed(2);
+  document.getElementById("total12").textContent = total12.toFixed(2);
+  document.getElementById("total13").textContent = total13.toFixed(2);
+  document.getElementById("total14").textContent = total14.toFixed(2);
+  document.getElementById("total15").textContent = total15.toFixed(2);
+  document.getElementById("total16").textContent = total16.toFixed(2);
+  document.getElementById("total17").textContent = total17.toFixed(2);
+  document.getElementById("total18").textContent = total18.toFixed(2);
+  document.getElementById("total19").textContent = total19.toFixed(2);
+  document.getElementById("total20").textContent = total20.toFixed(2);
+  document.getElementById("total21").textContent = total21.toFixed(2);
+  document.getElementById("total22").textContent = total22.toFixed(2);
+  document.getElementById("total23").textContent = total23.toFixed(2);
+  document.getElementById("total24").textContent = total24.toFixed(2);
+  document.getElementById("total25").textContent = total25.toFixed(2);
+  document.getElementById("total26").textContent = total26.toFixed(2);
+  document.getElementById("total27").textContent = total27.toFixed(2);
+  document.getElementById("total28").textContent = total28.toFixed(2);
+  document.getElementById("total29").textContent = total29.toFixed(2);
+  document.getElementById("total30").textContent = total30.toFixed(2);
+  document.getElementById("total31").textContent = total31.toFixed(2);
+
+  //adding grand total
+  const grandTotalMorning =
+    price1 +
+    price3 +
+    price5 +
+    price7 +
+    price9 +
+    price11 +
+    price13 +
+    price15 +
+    price17 +
+    price19 +
+    price21 +
+    price23 +
+    price25 +
+    price27 +
+    price29 +
+    price31 +
+    price33 +
+    price35 +
+    price37 +
+    price39 +
+    price41 +
+    price43 +
+    price45 +
+    price47 +
+    price49 +
+    price51 +
+    price53 +
+    price55 +
+    price57 +
+    price59 +
+    price61;
+  const grandTotalEvening =
+    price2 +
+    price4 +
+    price6 +
+    price8 +
+    price10 +
+    price12 +
+    price14 +
+    price16 +
+    price18 +
+    price20 +
+    price22 +
+    price24 +
+    price26 +
+    price28 +
+    price30 +
+    price32 +
+    price34 +
+    price36 +
+    price38 +
+    price40 +
+    price42 +
+    price44 +
+    price46 +
+    price48 +
+    price50 +
+    price52 +
+    price54 +
+    price56 +
+    price58 +
+    price60 +
+    price62;
+
+  const grandTotalPrice = grandTotalMorning + grandTotalEvening;
+// showing Gtotal
+document.getElementById("grand-total-morning").textContent = grandTotalMorning.toFixed(2);
+document.getElementById("grand-total-evening").textContent = grandTotalEvening.toFixed(2);
+document.getElementById("grand-total-price").textContent = grandTotalPrice.toFixed(2);
+
+
+}
